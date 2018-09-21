cf958a3fbd8059600a7e8d3ff678899e22561dbf
Brandon VanCamp <brandon@overtimewebdev.com>
2018-09-21 13:10:30 -0500

    Add watch for changes in sass files.

--- gulpfile.js
+++ gulpfile.js
@@ -4,33 +4,41 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
+    maps = require('gulp-sourcemaps'),
     sass = require('gulp-sass');
 
 // Concat files.
 gulp.task("concatScripts", function() {
-   gulp.src([
+   return gulp.src([
        'js/circle/circle.js',
        'js/circle/autogrow.js'
        ])
+   .pipe(maps.init())
    .pipe(concat('global.js'))
-   .pipe(gulp.dest('js'));
-});
-
-
-// Compile Sass to css
-gulp.task('compileSass', function() {
-   gulp.src('sass/global.scss')
-   .pipe(sass())
-   .pipe(gulp.dest('css'));
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('js'));
 });
 
 // Minify Files
-gulp.task("minifyScripts", function() {
-   gulp.src('js/global.js')
+gulp.task("minifyScripts", ['concatScripts'], function() {
+   return gulp.src('js/global.js')
        .pipe(uglify())
        .pipe(rename('global.min.js'))
        .pipe(gulp.dest('js'))
 });
+
+// Compile Sass to css
+gulp.task('compileSass', function() {
+   return gulp.src('sass/global.scss')
+   .pipe(sass())
+   .pipe(gulp.dest('css'));
+});
+
+// Watch for changes of the sass files.
+gulp.task('watchSass', function() {
+   gulp.watch('scss/**/*.scss', ['compileSass']);
+ })
+
 
 // 
 gulp.task('default', ['compileSass', 'concatScripts', 'minifyScripts'], function() {
